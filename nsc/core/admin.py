336f8fdb42ed008390189dc047e14942a2504e41
Stuart MacKay <smackay@flagstonesoftware.com>
2020-01-27 15:35:27 +0000

    Added the Condition model to the Django Admin.

--- nsc/core/admin.py
+++ nsc/core/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from .filters import AgeGroupFilter
 from .models import Condition
 
-admin.site.register(Condition)
+
+@admin.register(Condition)
+class ConditionAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'is_screened', 'ages_display')
+    list_filter = ('is_screened', AgeGroupFilter)
+
+    search_fields = ('name',)
+
+    def ages_display(self, obj):
+        return ', '.join(str(Condition.AGE_GROUP_LOOKUP[age]) for age in obj.ages)
+    ages_display.short_description = _('Ages')
