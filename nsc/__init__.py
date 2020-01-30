82a80f2979af63b6fc7227e4b62cff79d5f01d2c
Stuart MacKay <smackay@flagstonesoftware.com>
2020-01-30 16:54:58 +0000

    Re-added the part of the core app that contained the Condition model.
    The condition app is stripped down so it only contains the model,
    django admin and related code. The view and related code were removed
    as it is not clear that viewing and editing instances of the Condition
    model are going to be needed right now. However there is condition
    specific content - the description of the condition and the link to
    more information on the main National Health Service web site, so it
    seems likely that views will be added in the future.

--- nsc/__init__.py
+++ nsc/__init__.py
@@ -1,7 +1,24 @@
 from __future__ import absolute_import, unicode_literals
 
+from model_bakery import baker
+
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
+
+
+# A custom ArrayField was used to model the ages field on the Condition model
+# so a better default widget would be available. While model bakery supports
+# ArrayField it can't customized versions so we have to add a generator
+# function that can be used to generate fake data. Ideally we should use the
+# choices used on the model but that runs the risk of circular dependencies
+# unless the choices are factored out but that makes things more obscure /
+# complicated than necessary so we just use a hard-wired default value.
+
+def generate_ages():
+    return '{all}'
+
+
+baker.generators.add('nsc.condition.fields.ChoiceArrayField', 'nsc.generate_ages')
---
+++ nsc/condition/filters.py
@@ -0,0 +1,42 @@
+from django.contrib.admin.filters import SimpleListFilter
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Condition
+
+
+class AgeGroupFilter(SimpleListFilter):
+    """Filter for the list of possible ages on a Condition.
+
+    TODO
+        * Current the choices method is overridden to allow conditions that
+          specifically affect all ages to be distinguished from any condition.
+          It might be better to replace this with a checklist filter so the
+          use can specify more than one condition or revisit the model and
+          decide on how 'None' and 'all ages' relate to each other i.e. does
+          None mean 'not set' or 'all ages'.
+
+    """
+    title = _('Age Groups')
+    parameter_name = 'ages'
+
+    def lookups(self, request, model_admin):
+        return Condition.AGE_GROUPS
+
+    def choices(self, changelist):
+        yield {
+            'selected': self.value() is None,
+            'query_string': changelist.get_query_string(remove=[self.parameter_name]),
+            'display': _('Any age group'),
+        }
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == str(lookup),
+                'query_string': changelist.get_query_string({self.parameter_name: lookup}),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        lookup_value = self.value()
+        if lookup_value:
+            queryset = queryset.filter(ages__contains=[lookup_value])
+        return queryset
