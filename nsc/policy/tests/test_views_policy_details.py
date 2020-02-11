90544a2003a7b9b3c7b272b32f7b409ac076ab17
Stuart MacKay <smackay@flagstonesoftware.com>
2020-02-11 18:38:04 +0000

    Updated the tests.
    1. Added a test to catch the case where the PolicyForm was passing
       an empty string to the model DateField when the value should have
       been converted to None during cleaning.
    
    2. Split the test for the preview and publish pages to check the
       form fields on the preview page are hidden and simplify the
       test for checking the Policy model is updated. The previous
       test was a bit obscure.
    
    3. The back line on the Policy detail page no longer needs to preserve
       the search results so the test was simplified to reflect this.

--- nsc/policy/tests/test_views_policy_details.py
+++ nsc/policy/tests/test_views_policy_details.py
@@ -15,19 +15,33 @@ def test_detail_view(django_app):
     Test that we can view an instance via the detail view.
     """
     instance = baker.make(Policy)
-    response = django_app.get(reverse("policy:edit", kwargs={"slug": instance.slug}))
+    response = django_app.get(instance.get_admin_url())
     assert response.context["policy"] == instance
 
 
 def test_back_link(django_app):
     """
-    Test the back link returns to the previous page. This ensures that search results
-    are not lost.
+    Test the back link returns to the policy list page.
+    """
+    instance = baker.make(Policy, name="condition", ages="{child}")
+    detail = django_app.get(instance.get_admin_url())
+    results = detail.click(linkid="back-link-id")
+    assert results.request.path == reverse("policy:list")
+
+
+def test_back_link_discards_search_results(django_app):
+    """
+    Test returning to the policy list page discards previous search results.
+
+    NOTE: In the current design, search is only used to find a condition. The
+    search results do not need to be maintained when returning to the policy
+    list page.
     """
     instance = baker.make(Policy, name="condition", ages="{child}")
     form = django_app.get(reverse("policy:list")).form
     form["name"] = "condition"
     results = form.submit()
     detail = results.click(href=instance.get_admin_url())
-    referer = detail.click(linkid="back-link-id")
-    assert results.request.url == referer.request.url
+    results = detail.click(linkid="back-link-id")
+    assert results.request.path == reverse("policy:list")
+    assert results.request.environ["QUERY_STRING"] == ""
