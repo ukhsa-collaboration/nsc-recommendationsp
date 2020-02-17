4599d9c5786b75968f2f19a32f1c563dc7a8fce8
Richard Terry <git@radiac.net>
2020-02-17 10:35:34 +0000

    Fixed frontend paths

--- webpack.config.js
+++ webpack.config.js
@@ -77,6 +77,7 @@ const config = {
   devtool: 'source-map',
   devServer: {
     contentBase: path.resolve(__dirname, pathDist),
+    contentBasePublicPath: '/static/',
     hot: true,
     disableHostCheck: true,
     port: devServerPort,
