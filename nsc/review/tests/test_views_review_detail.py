6a3487c2ffe78c16966071713ed3aa748fe5e986
Stuart MacKay <smackay@flagstonesoftware.com>
2020-03-18 16:03:35 +0000

    Updated the review detail page to match the V8 prototype.
    There are still some gaps:
    
    1. The list of events showing the changes made, displayed at the top of the
       page is not implemented as it is not defined.
    
    2. The uploasd product history link in the post-consultation section is not
       implemented as it should be possible to scrape this information from the
       legacy/clone site.
    
    Both these issues and some others are covered in the Development Issues
    document on Google.

--- nsc/review/tests/test_views_review_detail.py
+++ nsc/review/tests/test_views_review_detail.py
@@ -1,4 +1,7 @@
+from django.urls import reverse
+
 import pytest
+from bs4 import BeautifulSoup
 
 
 # All tests require the database
@@ -6,10 +9,28 @@ pytestmark = pytest.mark.django_db
 pytest_plugins = ["nsc.review.tests.fixtures"]
 
 
-def test_view(make_review, django_app):
+@pytest.fixture
+def response(make_review, django_app):
+    review = make_review()
+    url = reverse("review:detail", kwargs={"slug": review.slug})
+    return django_app.get(url)
+
+
+@pytest.fixture
+def dom(response):
+    return BeautifulSoup(response.content, "html.parser")
+
+
+def test_view(response):
     """
     Test that the page can be displayed.
     """
-    review = make_review()
-    response = django_app.get(review.get_absolute_url())
     assert response.status == "200 OK"
+
+
+def test_back_link(dom):
+    """
+    Test the back link returns to the dashboard.
+    """
+    link = dom.find(id="back-link-id")
+    assert link["href"] == reverse("dashboard")
