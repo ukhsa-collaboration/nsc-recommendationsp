303bf7b761faab682a27dae06957882c0affbd4d
Stuart MacKay <smackay@flagstonesoftware.com>
2020-03-18 16:26:19 +0000

    Updated the (undocumented) delete confirmation page for a Review

--- nsc/review/tests/test_views_review_delete.py
+++ nsc/review/tests/test_views_review_delete.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 import pytest
+from bs4 import BeautifulSoup
 
 
 # All tests require the database
@@ -8,10 +9,29 @@ pytestmark = pytest.mark.django_db
 pytest_plugins = ["nsc.review.tests.fixtures"]
 
 
-def test_view(make_review, django_app):
+@pytest.fixture
+def response(make_review, django_app):
+    review = make_review()
+    url = reverse("review:delete", kwargs={"slug": review.slug})
+    return django_app.get(url)
+
+
+@pytest.fixture
+def dom(response):
+    return BeautifulSoup(response.content, "html.parser")
+
+
+def test_view(response):
     """
     Test that the page can be displayed.
     """
-    review = make_review()
-    response = django_app.get(reverse("review:delete", kwargs={"slug": review.slug}))
     assert response.status == "200 OK"
+
+
+def test_back_link(response, dom):
+    """
+    Test the back link returns to the review detail page.
+    """
+    review = response.context["object"]
+    link = dom.find(id="back-link-id")
+    assert link["href"] == review.get_absolute_url()
