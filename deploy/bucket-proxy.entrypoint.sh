4e8024f96629d6a15108fe497e7ec733385d1326
Dan Bate <dan.bate@gmail.com>
2021-04-14 16:15:09 +0100

    added build config for the media bucket proxy

--- deploy/bucket-proxy.entrypoint.sh
+++ deploy/bucket-proxy.entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export BUCKET_NAME=`cat /var/secrets/s3/bucket-name`
+export BUCKET_ENDPOINT=`cat /var/secrets/s3/endpoint`
+export BUCKET_HOST="${BUCKET_NAME}.${BUCKET_ENDPOINT}"
+
+echo "Proxying: ${BUCKET_HOST}"
 envsubst '$BUCKET_HOST' < ./nginx-cfg/bucket-proxy.nginx.config > $NGINX_CONF_TARGET
 
 nginx -g 'daemon off;'
