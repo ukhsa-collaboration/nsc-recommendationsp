0262d9e46bbf507602d71fa49366c44caf6e0bfd
Dan Bate <dan.bate@gmail.com>
2021-05-17 13:00:11 +0100

    fixed pagination in the public listing forgetting filters

--- nsc/utils/templatetags/utils.py
+++ nsc/utils/templatetags/utils.py
@@ -1,3 +1,5 @@
+from urllib.parse import parse_qs, urlencode, urlsplit
+
 from django import template
 
 
@@ -7,3 +9,18 @@ register = template.Library()
 @register.filter
 def get(obj, prop):
     return getattr(obj, prop, None)
+
+
+@register.simple_tag(takes_context=True)
+def update_qs(context, url=None, **params):
+    url = url or context.request.build_absolute_uri()
+
+    # split up the existing url
+    split = urlsplit(url)
+
+    # replace all the prams from the original url with the
+    # new ones appending new params
+    orig_params = parse_qs(split.query)
+    updated_params = {**orig_params, **params}
+
+    return f"{split.scheme}://{split.netloc}{split.path}?{urlencode(updated_params, doseq=True)}"
