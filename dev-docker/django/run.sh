df06e1e8b09552e6982cfc014200cdfad00241f4
Owusu <benita.owusu@accenture.com>
2025-05-08 08:13:30 +0100

    Update Docker setup and supporting files

--- dev-docker/django/run.sh
+++ dev-docker/django/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+set -e  # Exit on error
+
 cd /project
 
+echo "Waiting for database to be ready..."
+
+# Wait until PostgreSQL is accepting connections
+until pg_isready -h "$DATABASE_HOST" -p 5432 -U "$POSTGRES_USER"; do
+  echo "Waiting for postgres at $DATABASE_HOST:5432..."
+  sleep 2
+done
+
+echo "Database is up. Running migrations..."
 ./manage.py migrate
-./manage.py runserver 0:8000
 
-
+echo "Starting Django development server..."
+./manage.py runserver 0.0.0.0:8000
