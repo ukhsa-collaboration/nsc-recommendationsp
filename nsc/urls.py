51fe174db3adb6c20314734bca2539eb3fbe6491
juhiupretiNSC <juhi.upreti@ukhsa.gov.uk>
2025-06-09 17:20:25 +0100

    Remove url change

--- nsc/urls.py
+++ nsc/urls.py
@@ -43,11 +43,12 @@ if settings.AUTH_USE_ACTIVE_DIRECTORY:
     ]
 else:
     urlpatterns += [
-        path(
-            "accounts/login/",
-            auth_views.LoginView.as_view(template_name="registration/login.html"),
-            name="login",
-        ),
+        # path(
+        #     "accounts/login/",
+        #     auth_views.LoginView.as_view(template_name="registration/login.html"),
+        #     name="login",
+        # ),
+        path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
         path(
             "accounts/logout/",
             auth_views.LogoutView.as_view(next_page=reverse_lazy("login")),
