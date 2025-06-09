3907fddf9b7295a67e442a5ed69044d7414c497f
Benitaowusu <?>
2025-06-09 15:07:50 +0100

    test fix

--- nsc/notify/tests/fixtures.py
+++ nsc/notify/tests/fixtures.py
@@ -8,7 +8,8 @@ from ..models import Email
 
 @pytest.fixture(autouse=True)
 def notify_client_mock():
-    with patch("nsc.notify.client.client") as mock_client:
+    with patch("nsc.notify.client.get_client") as mock_get_client:
+        mock_client = mock_get_client.return_value
         yield mock_client
 
 
