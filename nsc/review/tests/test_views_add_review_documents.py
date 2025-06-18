f6d7cf45898eced07eb56d61733bfb4022bc778d
juhiupretiNSC <juhi.upreti@ukhsa.gov.uk>
2025-06-18 16:32:24 +0100

    trying test fix

--- nsc/review/tests/test_views_add_review_documents.py
+++ nsc/review/tests/test_views_add_review_documents.py
@@ -8,13 +8,13 @@ from nsc.review.models import Review
 # All tests require the database
 pytestmark = pytest.mark.django_db
 
-@pytest.fixture(autouse=True)
-def patch_virus_scanner(monkeypatch):
-    monkeypatch.setattr("nsc.utils.virus_scanner.is_file_clean", lambda f: True)
+# @pytest.fixture(autouse=True)
+# def patch_virus_scanner(monkeypatch):
+#     monkeypatch.setattr("nsc.utils.virus_scanner.is_file_clean", lambda f: True)
 
-def test_patch(monkeypatch):
-    from nsc.utils.virus_scanner import is_file_clean
-    assert is_file_clean("dummy") is True
+# def test_patch(monkeypatch):
+#     from nsc.utils.virus_scanner import is_file_clean
+#     assert is_file_clean("dummy") is True
 
 def test_view(erm_user, make_review, django_app):
     """
@@ -41,33 +41,24 @@ def test_view__incorrect_permission(make_review, test_access_forbidden):
         url=reverse("review:add-review-documents", kwargs={"slug": review.slug}),
     )
 
-
+@patch("nsc.utils.virus_scanner.is_file_clean", return_value=True)
 def test_success_url(erm_user, make_review, django_app, minimal_pdf):
     """
     Test success url on submit.
     """
-    # review = make_review(slug="abc")
-    # response = django_app.get(
-    #     reverse("review:add-review-documents", kwargs={"slug": review.slug}),
-    #     user=erm_user,
-    # )
-    # form = response.forms[1]
-    # form["cover_sheet"] = (
-    #     "document.pdf",
-    #     minimal_pdf.encode(),
-    #     "application/pdf",
-    # )
-    # actual = form.submit().follow()
-    # assert actual.request.path == reverse("review:detail", kwargs={"slug": review.slug})
+    review = make_review(slug="abc")
     response = django_app.get(
         reverse("review:add-review-documents", kwargs={"slug": review.slug}),
         user=erm_user,
     )
     form = response.forms[1]
-    form["cover_sheet"] = ("document.pdf", minimal_pdf.encode(), "application/pdf")
-    submit_response = form.submit()
-    print('submit res status:', submit_response.status)
-    print('submit res text', submit_response.text)  # Look for form errors in HTML
+    form["cover_sheet"] = (
+        "document.pdf",
+        minimal_pdf.encode(),
+        "application/pdf",
+    )
+    actual = form.submit().follow()
+    assert actual.request.path == reverse("review:detail", kwargs={"slug": review.slug})
 
 
 def test_success_url__next(erm_user, make_review, django_app, minimal_pdf):
