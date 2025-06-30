c9de0c318bdcbe845a0a0384111ab3a8c7e14fd8
Hakim <shara.hakim@accenture.com>
2025-06-30 15:24:51 +0100

    added a RuntimeError message for when no values are set in the django_admin_ip_ranges environment variable

--- nsc/settings.py
+++ nsc/settings.py
@@ -140,7 +140,12 @@ class Common(Configuration):
     ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS", cast=csv_to_list, default=["*"])
 
     # String containing ip ranges allowed to access django-admin
-    DJANGO_ADMIN_IP_RANGES = get_env("DJANGO_ADMIN_IP_RANGES", default="")
+    DJANGO_ADMIN_IP_RANGES = get_env("DJANGO_ADMIN_IP_RANGES")
+
+    if not DJANGO_ADMIN_IP_RANGES:
+        raise RuntimeError(
+            "DJANGO_ADMIN_IP_RANGES environment variable is required but not set. If you want django-admin to be accessible to anyone on the internet (restrict no one), then set it to 0.0.0.0/0"
+        )
 
     INSTALLED_APPS = [
         "django.contrib.admin",
