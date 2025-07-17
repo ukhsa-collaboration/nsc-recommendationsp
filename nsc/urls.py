87d9f1674fe660ff7cf642ed68219e0db1ffb4b2
juhiupretiNSC <juhi.upreti@ukhsa.gov.uk>
2025-07-17 15:35:31 +0100

    Fix formatting

--- nsc/urls.py
+++ nsc/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth import views as auth_views
 from django.http import HttpResponse
 from django.urls import include, path
 from django.views.generic import TemplateView
-from nsc.views import custom_admin_logout
 
 from nsc.review.views import ReviewDashboardView
+from nsc.views import custom_admin_logout
 
 
 admin.autodiscover()
@@ -23,7 +23,7 @@ urlpatterns = [
         name="feedback",
     ),
     path(r"admin/", ReviewDashboardView.as_view(), name="dashboard"),
-    path(r"django-admin/logout/", custom_admin_logout, name='admin-logout'),
+    path(r"django-admin/logout/", custom_admin_logout, name="admin-logout"),
     path(r"contact/", include("nsc.contact.urls", namespace="contact")),
     path(r"document/", include("nsc.document.urls", namespace="document")),
     path(r"stakeholder/", include("nsc.stakeholder.urls", namespace="stakeholder")),
