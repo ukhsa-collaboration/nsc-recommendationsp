205cb596c2a821f3a48ddc9ba7bc7e66c579df27
kathryn-dale <161315272+kathryn-dale@users.noreply.github.com>
2025-09-03 13:54:43 +0000

    INC0191840/LOPS3-605: Add logging (#255)
    * INC0191840/LOPS3-605: Add logging
    
    * Update README.md
    
    Co-authored-by: emilyjevans <30391244+emilyjevans@users.noreply.github.com>
    
    * Delete old readme
    
    ---------
    
    Co-authored-by: emilyjevans <30391244+emilyjevans@users.noreply.github.com>

--- nsc/notify/tasks.py
+++ nsc/notify/tasks.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 @app.task
 def send_pending_emails():
     # limit to the first 3000 as a max of 3000 messages can be sent in a minute
+    logger.info("Sending pending emails")
     for email in Email.objects.to_send()[:3000]:
         try:
             email.send()
@@ -19,6 +20,7 @@ def send_pending_emails():
 
 @app.task
 def update_stale_email_statuses():
+    logger.info("Updating stale email statuses")
     for email in Email.objects.sending().stale().with_notify_id():
         try:
             email.update_status()
